5aa2435bf5e965811cb5d6e846516f8ea3934a5a
AuthenticCode313 <80369849+AuthenticCode313@users.noreply.github.com>
2021-04-15 17:22:28 -0700

    First commit

---
+++ DataStructuresLab/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DataStructuresLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a word you would like to reverse: ");
+            string input = Console.ReadLine();
+            string reversed = ReverseString(input);
+            Console.WriteLine(reversed);
+        }
+        static string ReverseString(string myString)
+        {
+            string reversed = "";
+            for(int i = myString.Length - 1; i >= 0; i--)
+            {
+                reversed += myString[i];
+            }
+            return reversed;
+        }
+    }
+}
